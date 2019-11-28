0f510f9bc924b65bacdea23f449503a359175f14
ironromeo <ironromeo@gmail.com>
2019-11-28 23:58:18 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindStickPlot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxExcelFilename = new System.Windows.Forms.TextBox();
+            this.textBoxBeginDate = new System.Windows.Forms.TextBox();
+            this.textBoxEndDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNorthDirection = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxInterpolateMultiplier = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxStrength = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(502, 12);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(199, 55);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxExcelFilename
+            // 
+            this.textBoxExcelFilename.Location = new System.Drawing.Point(160, 12);
+            this.textBoxExcelFilename.Name = "textBoxExcelFilename";
+            this.textBoxExcelFilename.Size = new System.Drawing.Size(336, 20);
+            this.textBoxExcelFilename.TabIndex = 1;
+            this.textBoxExcelFilename.Click += new System.EventHandler(this.textBoxExcelFilename_Click);
+            // 
+            // textBoxBeginDate
+            // 
+            this.textBoxBeginDate.Location = new System.Drawing.Point(160, 38);
+            this.textBoxBeginDate.Name = "textBoxBeginDate";
+            this.textBoxBeginDate.Size = new System.Drawing.Size(82, 20);
+            this.textBoxBeginDate.TabIndex = 2;
+            // 
+            // textBoxEndDate
+            // 
+            this.textBoxEndDate.Location = new System.Drawing.Point(160, 64);
+            this.textBoxEndDate.Name = "textBoxEndDate";
+            this.textBoxEndDate.Size = new System.Drawing.Size(82, 20);
+            this.textBoxEndDate.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Файл EXCEL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Дата начала";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Дата конец";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Направление СЕВЕРА";
+            // 
+            // textBoxNorthDirection
+            // 
+            this.textBoxNorthDirection.Location = new System.Drawing.Point(160, 106);
+            this.textBoxNorthDirection.Name = "textBoxNorthDirection";
+            this.textBoxNorthDirection.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNorthDirection.TabIndex = 7;
+            this.textBoxNorthDirection.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "интерполяция(множитель)";
+            // 
+            // textBoxInterpolateMultiplier
+            // 
+            this.textBoxInterpolateMultiplier.Location = new System.Drawing.Point(160, 137);
+            this.textBoxInterpolateMultiplier.Name = "textBoxInterpolateMultiplier";
+            this.textBoxInterpolateMultiplier.Size = new System.Drawing.Size(56, 20);
+            this.textBoxInterpolateMultiplier.TabIndex = 9;
+            this.textBoxInterpolateMultiplier.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ширина(пикселей)";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(160, 168);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(56, 20);
+            this.textBoxWidth.TabIndex = 11;
+            this.textBoxWidth.Text = "2000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "макс сила ветра";
+            // 
+            // textBoxStrength
+            // 
+            this.textBoxStrength.Location = new System.Drawing.Point(160, 195);
+            this.textBoxStrength.Name = "textBoxStrength";
+            this.textBoxStrength.Size = new System.Drawing.Size(56, 20);
+            this.textBoxStrength.TabIndex = 13;
+            this.textBoxStrength.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxStrength);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxInterpolateMultiplier);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxNorthDirection);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEndDate);
+            this.Controls.Add(this.textBoxBeginDate);
+            this.Controls.Add(this.textBoxExcelFilename);
+            this.Controls.Add(this.buttonOK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxExcelFilename;
+        private System.Windows.Forms.TextBox textBoxBeginDate;
+        private System.Windows.Forms.TextBox textBoxEndDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNorthDirection;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxInterpolateMultiplier;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxStrength;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using Common.Tools.Parsers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindStickPlot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class WindRecord
+        {
+            public DateTime Date;
+            public double Strength;
+            public double Direction;
+
+            public double x;
+            public double rad;
+            public double x2;
+            public double y;
+            public override string ToString()
+            {
+                return $"{Date}:{Direction}={Strength}";
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            string fn = SrcFilename;
+            try
+            {
+                if (!Read(fn)) return;
+                if (!PrepareData(false)) return;
+                if (!RenderData()) return;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Ошибка " + exc.Message);
+                return;
+            }
+        }
+
+        WindRecord[] Data = null;
+        private bool Read(string fn)
+        {
+            ExcelTableReader reader = new ExcelTableReader();
+
+            if (!reader.Read(fn))
+            {
+                MessageBox.Show("Не удалось прочитать файл " + fn);
+                return false;
+            }
+
+            int c = 0;
+            if(reader.Pages.Count>1)
+            {
+                string mes = string.Format("Файл '{0}' содержит более одной закладки. Прочитать данные из первой закладки='{1}'?", Path.GetFileName(fn), reader.Pages[0].Name);
+                if (MessageBox.Show(mes, "", MessageBoxButtons.OKCancel) != DialogResult.OK) return false;
+            }
+            c = 0;
+
+            DataTable table = reader.Pages[c].Table;
+            if (table.Columns.IndexOf("date") == -1)
+            {
+                MessageBox.Show("Не найдена колонка date");
+                return false;
+            }
+            if (table.Columns.IndexOf("direction") == -1)
+            {
+                MessageBox.Show("Не найдена колонка direction");
+                return false;
+            }
+            if (table.Columns.IndexOf("strength") == -1)
+            {
+                MessageBox.Show("Не найдена колонка strength");
+                return false;
+            }
+
+            List<WindRecord> data = new List<WindRecord>();
+            List<string> errors = new List<string>();
+
+            foreach (DataRow row in table.Rows)
+            {
+                var item = new WindRecord();
+                if (!DateTime.TryParse(row["date"].ToString(), out DateTime dt))
+                {
+                    errors.Add("invalid date:" + row["date"].ToString());
+                    continue;
+                }
+                item.Date = dt;
+
+                if (!double.TryParse(row["direction"].ToString(), out double direction))
+                {
+                    errors.Add("invalid direction:" + row["direction"].ToString());
+                    continue;
+                }
+                item.Direction = direction;
+
+                if (!double.TryParse(row["strength"].ToString(), out double strength))
+                {
+                    errors.Add("invalid strength:" + row["strength"].ToString());
+                    continue;
+                }
+                item.Strength = strength;
+                data.Add(item);
+            }
+
+            Data = data.ToArray();
+
+
+            return true;
+        }
+
+        int InterpolateMultiplier = 2;
+        private WindRecord[] InterpolateData(WindRecord[] src)
+        {
+            List<WindRecord> result = new List<WindRecord>();
+            for (int c = 0; c < src.Length; c++)
+            {
+                var item = src[c];
+                result.Add(item);
+                if( c < src.Length-1)
+                {
+                    var nextItem = src[c+1];
+                    var intervalMinutes = (nextItem.Date - item.Date).TotalMinutes;
+                    for (int i = 1; i < InterpolateMultiplier; i ++ )
+                    {
+                        double ratio = (double)i / InterpolateMultiplier;
+                        var interDate = item.Date.AddMinutes(intervalMinutes* ratio);
+                        var interDirection = item.Direction + (nextItem.Direction - item.Direction) * ratio;
+                        var interS = item.Strength + (nextItem.Strength - item.Strength) * ratio;
+                        var iterpolated = new WindRecord() { Date = interDate, Direction = interDirection, Strength = interS };
+                        result.Add(iterpolated);
+                    }
+                }
+            }
+            return result.ToArray();
+        }
+
+        private bool PrepareData(bool updateDates)
+        {
+            int beginDateIndex = 0;
+            DateTime prevDate = DateTime.MinValue;
+            for (int c = 0; c < Data.Length; c++)
+            {
+                var curDate = Data[c].Date;
+
+                if (curDate == prevDate)
+                {
+                    continue;
+                }
+
+                if (beginDateIndex + 1 < c)
+                {
+                    var stepMinutes = 1440 / (c - beginDateIndex);
+                    for (int d = beginDateIndex; d < c; d++)
+                    {
+                        Data[d].Date = Data[d].Date.AddMinutes(stepMinutes * (d - beginDateIndex));
+                    }
+                }
+
+                beginDateIndex = c;
+                prevDate = curDate;
+            }
+
+            if (updateDates)
+            {
+                this.textBoxBeginDate.Text = this.Data.First().Date.ToString("dd.MM.yyyy");
+                this.textBoxEndDate.Text = this.Data.Last().Date.ToString("dd.MM.yyyy");
+
+                var maxWindStrength = this.Data.Max(x => x.Strength);
+                this.textBoxStrength.Text = maxWindStrength.ToString();
+            }
+
+            int renderedNorthDirection = int.Parse(this.textBoxNorthDirection.Text);
+            this.northDirectionAngle = -renderedNorthDirection + 90;
+
+            this.InterpolateMultiplier = int.Parse(this.textBoxInterpolateMultiplier.Text);
+            this.canvasWidth = int.Parse(this.textBoxWidth.Text);
+
+            return true;
+        }
+
+
+        int canvasWidth = 2000;
+        int canvasHeight = 400;
+        static int MARGIN = 200;
+        int maxStrengthLen = MARGIN;
+        double northDirectionAngle = 90;
+
+        private bool RenderData()
+        {
+            DateTime beginDate = DateTime.Parse(this.textBoxBeginDate.Text);
+            DateTime endDate = DateTime.Parse(this.textBoxEndDate.Text).AddHours(23);
+
+            var selectedData = Data.Where(x => x.Date >= beginDate && x.Date <= endDate).ToArray();
+            var renderingData = InterpolateData(selectedData);
+
+            var maxWindStrength = double.Parse(this.textBoxStrength.Text);
+
+            var totalMinutes = (endDate - beginDate).TotalMinutes;
+            var pixelXperMinute = (canvasWidth - 2 * MARGIN) / totalMinutes;
+            var pixelYperStrength = maxStrengthLen / maxWindStrength;
+
+            for (int c = 0; c < renderingData.Length; c++)
+            {
+                var item = renderingData[c];
+
+                item.x = pixelXperMinute * (item.Date - beginDate).TotalMinutes;
+                item.rad = (northDirectionAngle - item.Direction) * Math.PI / 180;
+                item.y = (pixelYperStrength * item.Strength) * Math.Sin(item.rad);
+                var x_shift = (pixelYperStrength * item.Strength) * Math.Cos(item.rad);
+                item.x2 = item.x + x_shift;
+            }
+
+            Image bmp = new Bitmap(canvasWidth, canvasHeight);
+            Graphics g = Graphics.FromImage(bmp);
+            var penAxis = new Pen(Color.Black);
+            var pen = new Pen(Color.Blue);
+            int G1_X0 = MARGIN;
+            int G1_Y0 = MARGIN;
+            g.DrawLine(penAxis, new Point(G1_X0, G1_Y0), new Point(canvasWidth - MARGIN, G1_Y0));
+
+            for (int i = 0; i < renderingData.Length; i++)
+            {
+                var item = renderingData[i];
+                g.DrawLine(pen, new Point(G1_X0 + (int)item.x, G1_Y0), new Point(G1_X0 + (int)item.x2, G1_Y0 - (int)item.y));
+            }
+
+
+            var penNorth = new Pen(Color.Red);
+            var northrad = (northDirectionAngle - 0) * Math.PI / 180;
+            var xNorth = Math.Cos(northrad) * 100;
+            var yNorth = Math.Sin(northrad) * 100;
+            int northX = 180;
+            g.DrawLine(penNorth, new Point(northX, G1_Y0), new Point(northX + (int)xNorth, G1_Y0- (int)yNorth));
+            int radius = 5;
+            g.DrawEllipse(penNorth, northX - radius, G1_Y0 - radius, radius * 2, radius * 2);
+
+            var penStrength = new Pen(Color.Green);
+            g.DrawLine(penStrength, new Point(G1_X0, G1_Y0), new Point(G1_X0, G1_Y0 - maxStrengthLen));
+            g.DrawLine(penStrength, new Point(canvasWidth - MARGIN, G1_Y0), new Point(canvasWidth - MARGIN, G1_Y0 - maxStrengthLen));
+
+            g.Dispose();
+
+            string resultFileName = string.Format("{0}_{1}_{2}_N{3}_x{4}.bmp", Path.GetFileNameWithoutExtension(SrcFilename), beginDate.ToString("dd-MM-yyyy"), endDate.ToString("dd-MM-yyyy"), this.textBoxNorthDirection.Text, InterpolateMultiplier);
+            string fn = Path.Combine(Path.GetDirectoryName(SrcFilename), resultFileName);
+            bmp.Save(fn);
+
+            System.Diagnostics.Process.Start(fn);
+            return true;
+        }
+
+        private void textBoxExcelFilename_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxExcelFilename.Text = this.openFileDialog1.FileName;
+                if (!Read(SrcFilename)) return;
+                if (!PrepareData(true)) return;
+
+            }
+        }
+
+        public string SrcFilename => this.textBoxExcelFilename.Text;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBoxExcelFilename.Text = @"C:\temp\wind\wind.xlsx";
+            if (!Read(SrcFilename)) return;
+            if (!PrepareData(true)) return;
+
+        }
+    }
+}
